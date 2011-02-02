734b5788601ee999bdb7c1526495a62f5f807cc1
zzzadams <zadams@capital.edu>
2011-02-02 17:16:06 -0500

    Empty Game files

---
+++ Game.h
@@ -0,0 +1,52 @@
+/*Game.cpp
+Initial version
+Zach Adams
+Feb. 02 2011*/
+
+
+#ifndef _GAME_H
+#define _GAME_H
+
+#include <iostream>
+using namespace std;
+
+class Game {
+    public:
+        int score_, ccount_, lcount_; //point total, coin total, life total
+        //int time_; do we even handle this?
+        Game();
+        int sCall(); //send score
+        int cCall(); //send coins
+        int lCall(); //send lives
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+};
+
+#endif
