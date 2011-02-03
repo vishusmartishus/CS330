56f76b0aee14af648edfd5f0dbbdfd9c6c0cc6e4
zzzadams <zadams@capital.edu>
2011-02-03 14:14:11 -0500

    fixed/completed Game class files

--- Game.cpp
+++ Game.cpp
@@ -3,41 +3,60 @@ Initial version
 Zach Adams
 Feb. 02 2011*/
 
-/*not included: 
-tracking time (is this even our responsibility?)
-adding to amount of coins
-adding to amount of points
-no default value for lives specified
-no way to alter number of lives */
-
 #include "game.h"
 
 Game::Game()
 {
-    score_=0; //sets score and coins to default values. not sure if this is the best way to handle this?
-    ccount_=0;
+    pointsTotal_=0;
+    coinsTotal_=0;
+    livesTotal_=3;
 }
 
-int Game::sCall()
+//------------------------------------------------------------
+
+int Game::pointsCall()
 {
-    return score_;
+    return pointsTotal_;
 }
 
-int Game::cCall()
+//------------------------------------------------------------
+
+int Game::coinsCall()
 {
-    return ccount_;
+    return coinsTotal_;
 }
 
-int Game::lCall()
+//------------------------------------------------------------
+
+int Game::livesCall()
 {
-    return lcount_;
+    return livesTotal_;
 }
 
+//------------------------------------------------------------
 
+void Game::addCoin()
+{
+    coinsTotal_++;
+    if (coinsTotal_==100) { //100 coins to life transition
+        addLife();
+        coinsTotal_=0;
+    }
+}
 
+//------------------------------------------------------------
 
+void Game::addLife()
+{
+    livesTotal_++;
+}
 
+//------------------------------------------------------------
 
+void Game::subLife()
+{
+    livesTotal_--;
+}
 
 
 
--- Game.h
+++ Game.h
@@ -1,4 +1,4 @@
-/*Game.cpp
+/*Game.h
 Initial version
 Zach Adams
 Feb. 02 2011*/
@@ -7,17 +7,18 @@ Feb. 02 2011*/
 #ifndef _GAME_H
 #define _GAME_H
 
-#include <iostream>
-using namespace std;
-
 class Game {
     public:
-        int score_, ccount_, lcount_; //point total, coin total, life total
-        //int time_; do we even handle this?
         Game();
-        int sCall(); //send score
-        int cCall(); //send coins
-        int lCall(); //send lives
+        int pointsCall(); //send score
+        int coinsCall(); //send coins
+        int livesCall(); //send lives
+        void addCoin(); //add a single coin to current total. also handles 100 coins=life transition
+        void addLife(); 
+        void subLife(); //subtract life
+    private:
+        int pointsTotal_, coinsTotal_, livesTotal_; //point total, coin total, life total
+        
 
 
 
