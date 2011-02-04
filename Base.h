b714b03728efe03350f5e7bd1e566529ad10a1ea
JakeShadowSteel <JakeShadowSteel@hotmail.com>
2011-02-04 17:35:25 -0500

    Added Base.h

--- Base.h
+++ Base.h
@@ -1,26 +1,25 @@
-/*
- *  Base.h
- *  Base
- *
- *  Created by Jamie Veals on 2/2/11.
- *  Copyright 2011 Capital University. All rights reserved.
- *
- */
-
+// Jamie Veals, Jay Bondzeleske
+//---------------------------------------------------------
+//If Base.h is not defined, defines it
 #ifndef _BASE_H
 #define _BASE_H
-
-#include <iostream>
-using namespace std;
-
+//---------------------------------------------------------
+// Base class
 class Base {
-private:
-    int left, right, bottom, top;
-    char color;
-    
+//---------------------------------------------------------    
 public:
-    //method to draw 
-    void draw();
-    
+    // OpenGL abstract method to draw object
+    virtual void draw()=0;
+    // Setters and getters for the left, right, top, and bottom
+    int left() const { return left_ }
+    int right() const { return right_ }
+    int top() const { return top_ }
+    int bottom() const { return bottom_ }
+//---------------------------------------------------------
+private:
+    // Private variables for the left, right, top, and bottom
+    int left_, right_, bottom_, top_;
+    // Array of three numbers for the color
+    float color_[3];    
 };
 #endif
