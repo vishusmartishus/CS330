e4634add0791fc97b036af88ecbf0763b75fc173
wizrides <jsampson@capital.edu>
2011-02-07 23:36:17 -0500

    add Fireball.h

---
+++ Walking.cpp
@@ -0,0 +1,26 @@
+/*
+ *  Walking.cpp
+ *  Movable
+ *
+ *  Created by Jeremy Sampson on 2/7/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "Walking.h"
+
+//---------------------------------------------------------
+
+Walking::Walking()
+{
+    
+}
+
+//---------------------------------------------------------
+
+Walking::~Walking()
+{
+    
+}
+
+//---------------------------------------------------------
