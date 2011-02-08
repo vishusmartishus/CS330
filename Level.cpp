25cb68208fe5c05b9ea034d1dd42e102124fb505
zzzadams <zadams@capital.edu>
2011-02-08 14:35:28 -0500

    Empty level files that compile

---
+++ Level.cpp
@@ -0,0 +1,13 @@
+/*Level.cpp (empty)
+Zach Adams
+Feb. 08 2011*/
+
+#include "Level.h"
+
+Level::Level()
+{
+
+
+
+
+}
---
+++ Level.h
@@ -0,0 +1,39 @@
+/*Level.h (empty)
+Zach Adams
+Feb. 08 2011
+Need to work things out with the rest of the group before defining any methods.
+*/
+
+
+
+#ifndef _LEVEL_H
+#define _LEVEL_H
+
+class Level {
+    public:
+        Level();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+#endif
