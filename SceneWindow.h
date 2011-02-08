4784bdad45cf2a8d9429623dd3d865b197802cc0
hydrosolar <jwhydrosolar@gmail.com>
2011-02-08 11:43:00 -0500

    Added SceneWindow.h and SceneWindow.cpp

---
+++ SceneWindow.h
@@ -0,0 +1,38 @@
+#ifndef __SCENEWINDOW_H__
+#define __SCENEWINDOW_H__
+
+//----------------------------------------------------------------------
+
+class SceneWindow {
+    
+public:
+    SceneWindow(int argc, char **argv);
+    
+    // static callbacks
+    static void displayFunc();
+    static void keyboardFunc(unsigned char key, int x, int y);
+    static void keyboardUpFunc(unsigned char key, int x, int y);
+    static void timerFunc(int value);
+    
+    // non-static callbacks
+    void displayCB();
+    void keyboardCB(unsigned char key, int x, int y);
+    void keyboardUpCB(unsigned char key, int x, int y);
+    void timerCB(int value);
+    
+    void startGame();
+    void mainLoop();
+    
+private:
+    Drawable position_[];
+    
+    // whole level size
+    int width_, height_;
+	// viewport size
+	int viewportWidth_, viewportHeight_;
+};
+
+//----------------------------------------------------------------------
+
+#endif
+
