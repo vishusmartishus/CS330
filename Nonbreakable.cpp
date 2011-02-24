61a0edb3faff021b84239551bed5cf4c1569766d
JakeShadowSteel <JakeShadowSteel@hotmail.com>
2011-02-23 21:35:06 -0500

    Nonbreakable.cpp with some work

--- Nonbreakable.cpp
+++ Nonbreakable.cpp
@@ -16,8 +16,9 @@ void Nonbreakable::draw()
 void Nonbreakable::create(int xCoor, int yCoor)
 {
 
-
-
+	//Nonbreakable::draw();
+	//Nonbreakable::setX(xCoor);
+	//Nonbreakable::setY(yCoor);
 
 
 
@@ -26,10 +27,22 @@ void Nonbreakable::create(int xCoor, int yCoor)
 //generateReward function
 void Nonbreakable::generateReward(bool isLarge, int reward)
 {
+	//reward ints, 1=coin, 2=mushroom/fireflower, 3=star
+	if (reward == 1) {
+		//reward will be a coin
+	}
+	else if (reward == 2) {
+		if (isLarge == true) {
+			// Reward will be fireflower
+		}	
+		else {
+			// Reward will be a mushroom
+		}	
+	}
+	else {
+		// reward will be a star
 
-
-
-
+			
 
 
 }   
@@ -39,7 +52,9 @@ void Nonbreakable::redraw(int xCoor, int yCoor)
 {
 
 
-
+	//Nonbreakable::draw();
+	//Nonbreakable::setX(xCoor);
+	//Nonbreakable::setY(yCoor);
 
 
 
