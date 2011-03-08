b74abcecfff92299df471565cd387f864f6cc24e
Theetails <theetails@gmail.com>
2011-03-08 06:40:53 -0500

    level.cpp now has code for opening and reading level file

--- Level.cpp
+++ Level.cpp
@@ -3,6 +3,9 @@
 
 #include "Level.h"
 #include "Drawable.h"
+#include <fstream>
+using namespace std;
+
 //------------------------------------------------------------
 Level::Level()
 {
@@ -14,6 +17,26 @@ Level::Level()
 //------------------------------------------------------------
 void Level::makeLevel()
 {
+	
+	ifstream inFile;
+	inFile.open("level/level1.txt");
+	
+	int xcord= -1, ycord= 18;
+	char object;
+	
+	while (inFile.get(object)) {
+		xcord++;
+		
+		/*at this point the varible 'object' has the next item in the .txt 
+		 and here the 'object' and it's xcord and ycord are sent off to the 
+		 correct LList*/
+		
+		if (xcord == 211) {
+			ycord --;
+			xcord =0;
+		}
+	}
+	
 
 
 
