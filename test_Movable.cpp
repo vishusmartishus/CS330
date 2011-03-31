d4c0b664ca38be37ad7d24970f067526d6b0cac3
Dave Reed <dave256@mac.com>
2011-03-31 13:03:37 -0400

    add test_Movable file and target

---
+++ test_Movable.cpp
@@ -0,0 +1,15 @@
+/*
+ *  test_Movable.cpp
+ *  Game
+ *
+ *  Created by David M. Reed on 3/31/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+int main()
+{
+
+	return 0;
+}
