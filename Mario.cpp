482bc201d8e7cb5ca89d8f054126fcce156ef3b7
hydrosolar <jwhydrosolar@gmail.com>
2011-04-07 16:09:11 -0400

    Draw Moving Mario sprites

--- Mario.cpp
+++ Mario.cpp
@@ -18,17 +18,27 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-GLuint textureMario;
+GLuint textureMario[10];
+int texturePos = 0;
 
 
 //------------------------------------------------------------
 void Mario::draw()
 {
+    
+    if (texturePos == 1) {
+        texturePos = 2;
+    }
+    else{
+        texturePos = 1;
+    }
+    
     glEnable( GL_TEXTURE_2D );
-    glBindTexture( GL_TEXTURE_2D, textureMario);
+    glBindTexture( GL_TEXTURE_2D, textureMario[texturePos]);
     
     glBegin( GL_QUADS );
     glTexCoord2d(0.0,0.0); glVertex2d(left(),bottom());
@@ -71,32 +81,49 @@ Mario::Mario()
         cHomeDir = getenv("HOMEPATH");
     }
     string homeDir = cHomeDir;
-    homeDir += "/CS330/sprites/mario.tex";
+    string iName;
+    homeDir += "/CS330/sprites/mario";
     
-    FILE *fp = fopen(homeDir.c_str(), "r");
-    unsigned char *texture = new unsigned char[4 * 256 * 256];
-    if (fread(texture, sizeof(unsigned char), 4 * 256 * 256, fp)
-        != 4* 256 *256) {
-        fprintf(stderr, "error reading %s", "sprites/mario.tex");
-    }
-    fclose(fp);
+    string pos;
+    stringstream out;
     
-    glGenTextures(1, &textureMario);
-    glBindTexture(GL_TEXTURE_2D, textureMario);
-    
-    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL );        
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                    GL_LINEAR_MIPMAP_NEAREST );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
-                    GL_LINEAR );        
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
-                    GL_CLAMP );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
-                    GL_CLAMP );
-    gluBuild2DMipmaps(GL_TEXTURE_2D, 4, 256, 256, GL_RGBA,
-                      GL_UNSIGNED_BYTE, texture);
-    delete [] texture;
+    for (int i = 0; i<3; ++i) {
+        stringstream out;
+        //Generates Filename
+        iName = homeDir;
+        out<<i;
+        pos = out.str();
+        cout<<pos<<endl;
+        iName += pos;
+        iName += ".tex";
+        
+        FILE *fp = fopen(iName.c_str(), "r");
+        unsigned char *texture = new unsigned char[4 * 256 * 256];
+        if (fread(texture, sizeof(unsigned char), 4 * 256 * 256, fp)
+            != 4* 256 *256) {
+            fprintf(stderr, "error reading %s", iName.c_str());
+        }
+        fclose(fp);
+        
+        glGenTextures(1, &textureMario[i]);
+        glBindTexture(GL_TEXTURE_2D, textureMario[i]);
+        
+        glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL );        
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                        GL_LINEAR_MIPMAP_NEAREST );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                        GL_LINEAR );        
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                        GL_CLAMP );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+                        GL_CLAMP );
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 4, 256, 256, GL_RGBA,
+                          GL_UNSIGNED_BYTE, texture);
+        delete [] texture;
 
+    }
+    
+   
 }
 //------------------------------------------------------------
 //updates Mario's movement info when a button is pushed
