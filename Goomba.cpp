997cb4d32e76cd27c706935d1ae5d6e1b3760d07
hydrosolar <jwhydrosolar@gmail.com>
2011-04-07 12:54:23 -0400

    Texture Draw Goomba

--- Goomba.cpp
+++ Goomba.cpp
@@ -12,6 +12,9 @@
 //---------------------------------------------------------
 
 #include "Goomba.h"
+#include <stdio.h>
+
+GLuint textureNum;
 
 //---------------------------------------------------------
 
@@ -23,6 +26,34 @@ Goomba::Goomba()
 	setPoints(0);
 	setXVelocity(2.0);
 	setYVelocity(0.0);
+    
+    
+    FILE *fp = fopen("/Users/rwatters/Desktop/goomba.tiff", "r");
+    unsigned char *texture = new unsigned char[4 * 16 * 16];
+    if (fread(texture, sizeof(unsigned char), 4 * 16 * 16, fp)
+        != 4* 16 *16) {
+        fprintf(stderr, "error reading %s", "/Users/rwatters/Desktop/goomba.tiff");
+    }
+    fclose(fp);
+    
+    
+    glGenTextures(1, &textureNum);
+    glBindTexture(GL_TEXTURE_2D, textureNum);
+    
+    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL );        
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_NEAREST );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                    GL_LINEAR );        
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                    GL_CLAMP );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+                    GL_CLAMP );
+    gluBuild2DMipmaps(GL_TEXTURE_2D, 4, 16, 16, GL_RGBA,
+                      GL_UNSIGNED_BYTE, texture);
+    delete [] texture;
+   
+
 }
 
 //---------------------------------------------------------
@@ -36,21 +67,18 @@ Goomba::~Goomba()
 
 void Goomba::draw()
 {
-	glColor3ub(205, 133, 63);
-    glBegin(GL_POLYGON);
-    glVertex2d(left(),bottom());
-    glVertex2d(right(),bottom());
-    glVertex2d(right(),top());
-    glVertex2d(left(),top());
+	    
+    glEnable( GL_TEXTURE_2D );
+    glBindTexture( GL_TEXTURE_2D, textureNum);
+    
+    glBegin( GL_QUADS );
+    glTexCoord2d(0.0,0.0); glVertex2d(left(),top());
+    glTexCoord2d(1.0,0.0); glVertex2d(right(),top());
+    glTexCoord2d(1.0,1.0); glVertex2d(right(),bottom());
+    glTexCoord2d(0.0,1.0); glVertex2d(left(),bottom());
     glEnd();
-    
-    
-    
-    
-
-    
-    
-    
+  
+    glDisable(GL_TEXTURE_2D);
     
 }
 
