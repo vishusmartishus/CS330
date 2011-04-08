db061cb62d69a21acf85a9ebda1ba13fba6d7bbd
hydrosolar <jwhydrosolar@gmail.com>
2011-04-07 23:30:39 -0400

    Now Draws Sprites with Alpha values

--- Goomba.cpp
+++ Goomba.cpp
@@ -106,17 +106,21 @@ void Goomba::draw()
     else{
         textureGoombaPos = 0;
     }
-	    
+	
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
     glEnable( GL_TEXTURE_2D );
     glBindTexture( GL_TEXTURE_2D, textureGoomba[textureGoombaPos]);
     
+    glColor4f(0.7f,0.9f,1.0f,1.0f);
     glBegin( GL_QUADS );
-    glTexCoord2d(0.0,0.0); glVertex2d(left(),top());
-    glTexCoord2d(1.0,0.0); glVertex2d(right(),top());
-    glTexCoord2d(1.0,1.0); glVertex2d(right(),bottom());
-    glTexCoord2d(0.0,1.0); glVertex2d(left(),bottom());
+    glTexCoord2d(0.0,0.0); glVertex2d(left(),bottom());
+    glTexCoord2d(1.0,0.0); glVertex2d(right(),bottom());
+    glTexCoord2d(1.0,1.0); glVertex2d(right(),top());
+    glTexCoord2d(0.0,1.0); glVertex2d(left(),top());
     glEnd();
   
+    glDisable(GL_BLEND);
     glDisable(GL_TEXTURE_2D);
     
 }
--- Mario.cpp
+++ Mario.cpp
@@ -60,10 +60,13 @@ void Mario::draw()
     
      
              
-    
+    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
     glEnable( GL_TEXTURE_2D );
     glBindTexture( GL_TEXTURE_2D, textureMario[textureMarioPos]);
     
+    
+    glColor4f(0.7f,0.9f,1.0f,1.0f);
     glBegin( GL_QUADS );
     glTexCoord2d(0.0,0.0); glVertex2d(left(),bottom());
     glTexCoord2d(1.0,0.0); glVertex2d(right(),bottom());
@@ -71,7 +74,9 @@ void Mario::draw()
     glTexCoord2d(0.0,1.0); glVertex2d(left(),top());
     glEnd();
     
-    glDisable(GL_TEXTURE_2D);}
+    glDisable(GL_BLEND);
+    glDisable(GL_TEXTURE_2D);
+}
 //------------------------------------------------------------
 //constructor for Mario Class
 Mario::Mario()
