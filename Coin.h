73b53b814bd88d41aaca6922716ffc4c7d23f9b8
JakeShadowSteel <JakeShadowSteel@hotmail.com>
2011-04-13 20:11:43 -0400

    Fixed up the code with better comments

--- Coin.h
+++ Coin.h
@@ -1,33 +1,33 @@
-// Coin.h
 // John (Jack) Johnson, Jamie Veals
 //------------------------------------------------------------
-// If Coin.h is not defined, defines it
+
 #ifndef _COIN_H
 #define _COIN_H
+
 //------------------------------------------------------------
-// Includes the Drawable.h file
+
 #include "Drawable.h"
+
 //------------------------------------------------------------
-// Class Coin that inherits from Drawable
+
 class Coin:public Drawable{
+
 //------------------------------------------------------------
+
 public:
-	// Constructor for Coin
 	Coin()
     {
 
-
-
-
-
-
     }
-	// Coin destructor
+
 	virtual ~Coin()
 	{
+
 	}
+
+	// OpenGL abstract method to draw object
 	virtual void draw();
-	// Returns object type
+	// Returns object type COIN
 	int objectType() { return COIN; }
 };
 
